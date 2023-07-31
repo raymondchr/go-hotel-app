d6d470b2001abe534f439f1f2df3d5269031d461
raymondchr <raycuffed@gmail.com>
2023-07-31 18:28:31 +0700

    Added middleware handling session manager

--- cmd/pkg/handlers/handlers.go
+++ cmd/pkg/handlers/handlers.go
@@ -10,8 +10,6 @@ import (
 
 var Repo *Repository
 
-
-
 type Repository struct {
 	App *config.AppConfig
 }
@@ -29,14 +27,18 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, this a string taken from backend"
-		
+	
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 	
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 			StringMap: stringMap,
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/raymondchr/go-hotel-app/cmd/pkg/config"
 	"github.com/raymondchr/go-hotel-app/cmd/pkg/handlers"
 	"github.com/raymondchr/go-hotel-app/cmd/pkg/render"
 )
 
+const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	//change to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	templateCache, err := render.CreateTemplateCache()
 	if err != nil {
@@ -26,9 +41,17 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println("The server is running on port 8080")
-	_ = http.ListenAndServe(":8080", nil)
+	// _ = http.ListenAndServe(":8080", nil)
+
+	srv := &http.Server{
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
